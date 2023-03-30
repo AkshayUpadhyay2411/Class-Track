d41cfdadda6136de00641bc3f07436314f7757a7
Ankit kumar <lit2020034@iiitl.ac.in>
2023-03-31 01:30:14 +0530

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        Hello moto
+      </header>
+    </div>
+  );
+}
+
+export default App;
